bb3ab3efd592a7966691090779b145536f361e47
Kid <hi@xuann.wang>
2026-06-02 22:25:13 +0800

    Init

---
+++ readme.md
@@ -0,0 +1,5 @@
+# HTML Preview
+
+> Cloudflare Workers backend for [Refined GitHub's `html-preview-link` feature](https://github.com/refined-github/refined-github/blob/main/source/features/html-preview-link.tsx)
+
+It is manually deployed to https://refined-github-html-preview.kidonng.workers.dev.
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import Shell from "./shell.html" with { type: "text" };
+
+const ShellPattern = new URLPattern({
+  pathname: "/:user/:repo/raw/:path(.+)",
+});
+const RenderPattern = new URLPattern({
+  pathname: "/_render/:user/:repo/raw/:path(.+)",
+});
+
+export default {
+  async fetch(request, env) {
+    const { url } = request;
+    const { pathname } = new URL(url);
+
+    if (pathname === "/") {
+      return Response.redirect(
+        "https://github.com/refined-github/refined-github/wiki/Extended-feature-descriptions#html-preview-link",
+      );
+    }
+
+    if (ShellPattern.test(url)) {
+      return new Response(Shell.replace("{{target}}", `/_render${pathname}`), {
+        headers: {
+          "content-type": "text/html; charset=utf-8",
+          // Disallow crawling
+          "x-robots-tag": "none",
+        },
+      });
+    }
+
+    const renderMatch = RenderPattern.exec(url);
+    if (!renderMatch) {
+      return new Response("Not Found", { status: 404 });
+    }
+
+    const isHtml = pathname.endsWith(".html") || pathname.endsWith(".htm");
+    if (isHtml && !request.headers.get("referer")?.startsWith(env.HOST)) {
+      return new Response("Forbidden", { status: 403 });
+    }
+
+    const { user, repo, path } = renderMatch.pathname.groups;
+    const response = await fetch(
+      `https://raw.githubusercontent.com/${user}/${repo}/${path}`,
+      request,
+    );
+    const headers = new Headers(response.headers);
+
+    // Correct content type
+    if (isHtml) {
+      headers.set("content-type", "text/html; charset=utf-8");
+    } else if (pathname.endsWith(".css")) {
+      headers.set("content-type", "text/css; charset=utf-8");
+    } else if (pathname.endsWith(".js")) {
+      headers.set("content-type", "text/javascript; charset=utf-8");
+    }
+
+    // Allow embedding
+    headers.set("x-frame-options", "sameorigin");
+    // Disable CSP
+    headers.delete("content-security-policy");
+    // Disallow crawling
+    headers.set("x-robots-tag", "none");
+
+    return new Response(response.body, {
+      headers,
+      status: response.status,
+      statusText: response.statusText,
+    });
+  },
+} satisfies ExportedHandler<Env>;
